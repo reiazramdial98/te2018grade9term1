6cba8cca2d5dbc23725a2eb026c0e0ba1e66db00
Reiaz Ramdial <reiazramdial98@gmail.com>
2018-10-13 15:41:52 -0400

    updated for the challenge

--- data.js
+++ data.js
@@ -1,5 +1,30 @@
 // Example
-let message = " shay is smart";
+let message = 24;
 console.log(message);
 
+let  number = 1300;
+console.log(number);
 // Type your code below!
+let  number2 = 1847;
+console.log(number2);
+
+let  pizza = "pizza";
+console.log(pizza);
+
+
+let  fish = "fish";
+console.log(fish);
+
+
+let  reiaz = "reiaz";
+console.log(reiaz);
+
+let  pet =false;
+console.log(pet);
+
+let  in9thgrade = true;
+console.log(in9thgrade);
+
+let  name ;
+console.log(name);
+
